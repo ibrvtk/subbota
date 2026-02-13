eabc9711d9eb3ed3a4e0ba685316f78cd11ea1c6
ibrvtk <nicecock@disroot.org>
2026-02-14 01:48:33 +0300

    Добавлена возможность улучшения стат

--- src/game/main.py
+++ src/game/main.py
@@ -1,4 +1,4 @@
-from random import random
+from random import random, choice
 
 
 TEXT_AREA = {
@@ -8,14 +8,31 @@ TEXT_AREA = {
     2: "тело",
     3: "ноги"
 }
+TEXT_PLAYER_PARAMS = {
+    'name': "Имя",
+    'hp_head': "Целостность головы",
+    'hp_body': "Целостность тела",
+    'hp_legs': "Целостность ног",
+    'hp': "Общее состояние здоровья",
+    'shield_power': "Прочность щита (поглощение урона от атаки)",
+    'shield_area': "Место на теле, защищаемое с помощью щита",
+    'armor': "Броня",
+    'damage': "Минимальный урон",
+    'weapon': "Оружие",
+    'crit_chance': "Шанс крит. удара",
+    'crit_damage': "Минимальный доп. урон при крит-е",
+    'points': "Очки прокачки"
+}
 
 # Словари для функций *attr()
 ATTR_HP = {
+    0: None,
     1: 'hp_head',
     2: 'hp_body',
     3: 'hp_legs'
 }
 ATTR_ARMOR = {
+    0: None,
     1: 'helmet',
     2: 'chestplate',
     3: 'greaves'
@@ -42,30 +59,32 @@ class Armor:
     '''
     Класс брони. Выдаётся игроку (`class Player`).
     Понижает получаемый урон.
-    Суммируется с щитом (`Player.shield`).
+    Суммируется с щитом (`Player.shield_power`).
     '''
     def __init__(self, name: str, helmet: int = 7, chestplate: int = 15, greaves: int = 7) -> None:
-        self.name = name
-        self.helmet = helmet
-        self.chestplate = chestplate
-        self.greaves = greaves
+        self.name: str = name
+        self.helmet: int = helmet
+        self.chestplate: int = chestplate
+        self.greaves: int = greaves
 
 class Player:
+    '''Homo Sapiens Sapiens'''
     def __init__(self, name: str, weapon: Weapon, armor: Armor,
                  hp_head: int = 100, hp_body: int = 100, hp_legs: int = 100,
-                 defense: int = 7, damage: int = 10, crit_chance: float = 0.3, crit_damage = 2.0) -> None:
-        self.name = name
-        self.hp_head = hp_head
-        self.hp_body = hp_body
-        self.hp_legs = hp_legs
-        self.hp = calc_hp(self)
-        self.shield = defense
-        self.armor = armor
-        self.shield_area = 0
-        self.damage = damage
-        self.weapon = weapon
-        self.crit_chance = crit_chance
-        self.crit_damage = crit_damage
+                 shield_power: int = 7, damage: int = 10, crit_chance: float = 0.3, crit_damage = 2.0) -> None:
+        self.name: str = name
+        self.hp_head: int = hp_head
+        self.hp_body: int = hp_body
+        self.hp_legs: int = hp_legs
+        self.hp: int = calc_hp(self)
+        self.shield_power: int = shield_power
+        self.shield_area: int = 0
+        self.armor: Armor = armor
+        self.damage: int = damage
+        self.weapon: Weapon = weapon
+        self.crit_chance: float = crit_chance
+        self.crit_damage: float = crit_damage
+        self.points: int = 10
 
     def crit(self) -> tuple[float, bool]:
         '''Подсчитывает и возвращает критический урон, основываясь на изначальных значениях (`self.crit_chance`, `self.crit_damage`).'''
@@ -95,7 +114,7 @@ class Player:
 
         if area == enemy.shield_area:
             # Если зона удара совпадает с зоной где у противника стоит щит, то отнять от значения урона значение силы щита противника
-            damage -= enemy.shield
+            damage -= enemy.shield_power
             text += " (защита)"
 
         setattr(enemy, area_hp, getattr(enemy, area_hp) - damage) # Отнять здоровье в размере урона (damage) у области куда был совершён удар
@@ -107,28 +126,53 @@ class Player:
         '''`self.shield_area = shield_area`'''
         self.shield_area = shield_area
 
+    def upgrade(self, param_to_upgrade: str) -> str:
+        restricted = ['name', 'hp', 'shield_area', 'armor', 'weapon', 'crit_chance', 'crit_damage', 'points']
+
+        if param_to_upgrade in restricted:
+            return "Вы не можете изменить этот параметр!"
+
+        if not hasattr(self, param_to_upgrade):
+            raise ValueError(f"Параметр {param_to_upgrade} не существует")
+
+        current_value = getattr(self, param_to_upgrade)
+        new_value = current_value + 1
+        setattr(self, param_to_upgrade, new_value)
+
+        self.points -= 1
+
+        return f"Параметр \"{TEXT_PLAYER_PARAMS[param_to_upgrade]}\" увеличен до {new_value}"
+
 
 # ТЕСТИРОВАНИЕ
 from random import randint
 
 
+Naked = Armor("Голое тело", 0, 0, 0)
 Chainmail = Armor("Кольчуга", chestplate=10, greaves=4)
 
+Fists = Weapon("Кулаки", 0, 0.0, 0.0)
 Claymore = Weapon("Клеймор", 10, 0.1, 1.1)
 Stiletto = Weapon("Стилет", 7, 0.4, 0.4)
 
-Timur = Player("Тимурджан", Claymore, Chainmail, damage=15)
-Gaine = Player("Генджел", Stiletto, Chainmail, crit_chance=0.35, crit_damage=1.9)
+Timur = Player("Тимурджан", Fists, Naked, damage=15)
+Diana = Player("Диана", Fists, Naked, crit_chance=0.35, crit_damage=1.9)
 
+
+while Timur.points != 0:
+    print(Timur.upgrade(choice(list(TEXT_PLAYER_PARAMS))))
+
+while Diana.points != 0:
+    print(Diana.upgrade(choice(list(TEXT_PLAYER_PARAMS))))
 
 while True:
     Timur.defense(randint(1, 3))
-    print(Gaine.attack(Timur, randint(1, 3)))
+    print(Diana.attack(Timur, randint(1, 3)))
     if Timur.hp <= 0:
-        print(f"{Gaine.name} победилa!")
+        print(f"{Diana.name} победилa!")
         break
-    Gaine.defense(randint(1, 3))
-    print(Timur.attack(Gaine, randint(1, 3)))
-    if Gaine.hp <= 0:
+    Diana.defense(randint(1, 3))
+    print(Timur.attack(Diana, randint(1, 3)))
+    if Diana.hp <= 0:
         print(f"{Timur.name} победил!")
         break
