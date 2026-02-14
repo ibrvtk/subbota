38f630cfec0776d32afbbf8b6352fac063862d34
ibrvtk <nicecock@disroot.org>
2026-02-14 19:43:25 +0300

    Добавлена броня (class ArmorElement)

--- src/game/main.py
+++ src/game/main.py
@@ -7,6 +7,11 @@ TEXT_AREA = {
     2: "корпус",
     3: "ноги"
 }
+TEXT_ARMORELEMENT_TYPE = {
+    'helmet': "шлем",
+    'chestplate': "нагрудник",
+    'greaves': "поножи"
+}
 TEXT_PLAYER_PARAMS = {
     'name': "Имя",
     'hp_head': "Целостность головы",
@@ -38,15 +43,36 @@ class Weapon:
 
 NoWeapon = Weapon("Без оружия", 0, 0.0, 0.0)
 
+class ArmorElement:
+    '''
+    Элемент брони.
+    * name: str - Название элемента брони (например "Железный шлем");
+    * type: str - Должен быть равен только ЛИБО `helmet` ЛИБО `chestplate` ЛИБО `greaves` (шлем, нагрудник, поножи);
+    * absorp(tion): int - Поглощение урона;
+    * dur(ability): int - Прочность (ЭКСПЕРИМЕНТАЛЬНО. ПОКА НЕ ИСПОЛЬЗУЕТСЯ). Можно оставить `None` (тогда никогда не поломается).
+    '''
+    def __init__(self, type: str, name: str, absorption: int, durability: int = None) -> None:
+        self.type: str = type
+        self.name: str = name
+        self.absorp: int = absorption
+        self.dur: int = durability
+
+NoHelmet = ArmorElement('helmet', "Без шлема", 0)
+NoChestplate = ArmorElement('chestplate', "Без нагрудника", 0)
+NoGreaves = ArmorElement('greaves', "Без поножей", 0)
+
 class Entity:
     def __init__(self, name: str, hp: int = 100,
                  damage: int = 10, weapon: Weapon = NoWeapon,
-                 armor: int = 10, crit_chance: float = 0.3, crit_damage = 2.0) -> None:
+                 helmet: ArmorElement = NoHelmet, chestplate: ArmorElement = NoChestplate, greaves: ArmorElement = NoGreaves,
+                 crit_chance: float = 0.3, crit_damage = 2.0) -> None:
         self.name: str = name
         self.hp: int = hp
         self.damage: int = damage
         self.weapon: Weapon = weapon
-        self.armor: int = armor
+        self.helmet: ArmorElement = helmet
+        self.chestplate: ArmorElement = chestplate
+        self.greaves: ArmorElement = greaves
         self.shield_area: int = 0
         self.crit_chance: float = crit_chance
         self.crit_damage: float = crit_damage
@@ -77,10 +103,19 @@ class Entity:
         else:
             crit_str = ""
 
-        damage = int(self.damage * crit - enemy.armor)  # урон * крит. урон - сила брони противника
+        damage = 0
+        match area:
+            # урон * крит. урон - сила брони противника
+            case 1:
+                damage = int((self.damage * crit) - enemy.helmet.absorp)
+            case 2:
+                damage = int((self.damage * crit) - enemy.chestplate.absorp)
+            case 3:
+                damage = int((self.damage * crit) - enemy.greaves.absorp)
+
         enemy.hp -= damage
 
-        return f"{self.name} нанес {enemy.name} {damage} ед. {crit_str}урона в {TEXT_AREA[area]}"
+        return f"{self.name} нанес {enemy.name} {damage} ед. {crit_str}урона ({TEXT_AREA[area]})"
 
     def defense(self, shield_area: int) -> None:
         '''
@@ -89,28 +124,34 @@ class Entity:
         '''
         self.shield_area = shield_area
 
-    def upgrade(self, param_to_upgrade: str) -> str:
-        restricted = ['name', 'hp', 'shield_area', 'armor', 'weapon', 'crit_chance', 'crit_damage', 'points']
-
-        if param_to_upgrade in restricted:
-            return "Вы не можете изменить этот параметр!"
-
+    def upgrade(self, param_to_upgrade: str, free: bool = False) -> str:
+        '''
+        Прокачивает integer-параметр, например дамаг или кол-во здоровья (в определённой зоне).
+        Снимает Player.points -= 1, если free не равен True.
+        '''
         if not hasattr(self, param_to_upgrade):
             raise ValueError(f"Параметр {param_to_upgrade} не существует")
 
+        restricted = ['name', 'hp', 'shield_area', 'armor', 'weapon', 'crit_chance', 'crit_damage', 'points'] # Недоступные для улучшения параметры
+        if param_to_upgrade in restricted:
+            return "Вы не можете изменить этот параметр!"
+
+        # Берём текущее значение требуемого параметра; прибавляем единицу; устанавливаем новое значение в класс
         current_value = getattr(self, param_to_upgrade)
         new_value = current_value + 1
         setattr(self, param_to_upgrade, new_value)
 
-        self.exp -= 1
+        if not free:
+            self.exp -= 1
 
         return f"Параметр \"{TEXT_PLAYER_PARAMS[param_to_upgrade]}\" увеличен до {new_value}"
+
 
 # ТЕСТИРОВАНИЕ
 Sword = Weapon("Меч", 10, 0.1, 1.0)
 
 Teamur = Entity("Тимурджан", damage=15, weapon=Sword)
-Diana = Entity("Диана", crit_chance=0.35, crit_damage=1.9)
+Diana = Entity("Диана", weapon=Sword, crit_chance=0.35, crit_damage=1.9)
 
 from random import randint
 while True:
